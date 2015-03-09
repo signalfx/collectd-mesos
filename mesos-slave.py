8c8b42f12ae8f12cd10be0df4e9ae4856d07ebf6
Ray Rodriguez <rayrod2030@gmail.com>
2015-03-09 12:30:58 -0400

    Adding some missing metrics and fixing a bunch of metric types to match types output by mesos

--- mesos-slave.py
+++ mesos-slave.py
@@ -34,27 +34,28 @@ STATS_CUR = {}
 STATS_MESOS = {
     # Slave
     'slave/frameworks_active': Stat("gauge", "slave/frameworks_active"),
-    'slave/invalid_framework_messages': Stat("gauge", "slave/invalid_framework_messages"),
-    'slave/invalid_status_updates': Stat("gauge", "slave/invalid_status_updates"),
-    'slave/recovery_errors': Stat("gauge", "slave/recovery_errors"),
+    'slave/invalid_framework_messages': Stat("counter", "slave/invalid_framework_messages"),
+    'slave/invalid_status_updates': Stat("counter", "slave/invalid_status_updates"),
+    'slave/recovery_errors': Stat("counter", "slave/recovery_errors"),
     'slave/registered': Stat("gauge", "slave/registered"),
-    'slave/tasks_failed': Stat("gauge", "slave/tasks_failed"),
-    'slave/tasks_finished': Stat("gauge", "slave/tasks_finished"),
-    'slave/tasks_killed': Stat("gauge", "slave/tasks_killed"),
-    'slave/tasks_lost': Stat("gauge", "slave/tasks_lost"),
+    'slave/tasks_failed': Stat("counter", "slave/tasks_failed"),
+    'slave/tasks_finished': Stat("counter", "slave/tasks_finished"),
+    'slave/tasks_killed': Stat("counter", "slave/tasks_killed"),
+    'slave/tasks_lost': Stat("counter", "slave/tasks_lost"),
     'slave/tasks_running': Stat("gauge", "slave/tasks_running"),
     'slave/tasks_starting': Stat("gauge", "slave/tasks_starting"),
+    'slave/tasks_staging': Stat("gauge", "slave/tasks_staging"),
     'slave/uptime_secs': Stat("gauge", "slave/uptime_secs"),
-    'slave/valid_framework_messages': Stat("gauge", "slave/valid_framework_messages"),
-    'slave/valid_status_updates': Stat("gauge", "slave/valid_status_updates"),
+    'slave/valid_framework_messages': Stat("counter", "slave/valid_framework_messages"),
+    'slave/valid_status_updates': Stat("counter", "slave/valid_status_updates"),
 
     # System
     'system/cpus_total': Stat("gauge", "system/cpus_total"),
     'system/load_15min': Stat("gauge", "system/load_15min"),
     'system/load_1min': Stat("gauge", "system/load_1min"),
     'system/load_5min': Stat("gauge", "system/load_5min"),
-    'system/mem_free_bytes': Stat("gauge", "system/mem_free_bytes"),
-    'system/mem_total_bytes': Stat("gauge", "system/mem_total_bytes")
+    'system/mem_free_bytes': Stat("bytes", "system/mem_free_bytes"),
+    'system/mem_total_bytes': Stat("bytes", "system/mem_total_bytes")
 }
 
 # DICT: Mesos 0.19.0, 0.19.1
@@ -65,8 +66,8 @@ STATS_MESOS_019 = {
 STATS_MESOS_020 = {
     'slave/executors_registering': Stat("gauge", "slave/executors_registering"),
     'slave/executors_running': Stat("gauge", "slave/executors_running"),
-    'slave/executors_terminated': Stat("gauge", "slave/executors_terminated"),
-    'slave/executors_terminating': Stat("gauge", "slave/executors_terminating")
+    'slave/executors_terminating': Stat("gauge", "slave/executors_terminating"),
+    'slave/executors_terminated': Stat("counter", "slave/executors_terminated")
 }
 
 # DICT: Mesos 0.21.0, 0.21.1
@@ -77,10 +78,13 @@ STATS_MESOS_021 = {
     'slave/disk_percent': Stat("gauge", "slave/disk_percent"),
     'slave/disk_total': Stat("gauge", "slave/disk_total"),
     'slave/disk_used': Stat("gauge", "slave/disk_used"),
+    'slave/mem_percent': Stat("gauge", "slave/mem_percent"),
+    'slave/mem_total': Stat("gauge", "slave/mem_total"),
+    'slave/mem_used': Stat("gauge", "slave/mem_used"),
     'slave/executors_registering': Stat("gauge", "slave/executors_registering"),
     'slave/executors_running': Stat("gauge", "slave/executors_running"),
-    'slave/executors_terminated': Stat("gauge", "slave/executors_terminated"),
-    'slave/executors_terminating': Stat("gauge", "slave/executors_terminating")
+    'slave/executors_terminating': Stat("gauge", "slave/executors_terminating"),
+    'slave/executors_terminated': Stat("counter", "slave/executors_terminated")
 }
 
 # FUNCTION: Collect stats from JSON result
